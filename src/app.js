17e798238562297b72ae6725e1987681c857cf98
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-08-18 16:20:59 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -13,4 +13,13 @@ app.use(express.json({limit: '16kb'}))
 app.use(express.urlencoded({limit: '16kb', extended: true}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import userRouter from "./routes/user.routes.js"
+app.use((req, res, next) => {
+    console.log(`Request Received: ${req.method} - ${req.path}`);
+    next();
+});
+// route declare
+app.use("/api/v1/users", userRouter)
+
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@
 
 // approach 2 of wrapping:
 const asyncHandler = (requestHandler)=>{
-    (req, res,next)=>{
+   return (req, res,next)=>{
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
